fa091420d01636bc40d8e7370904a88fec44b025
DanyloKuleshko <7dkuleshko@stpatrick.org>
2017-02-16 10:51:29 -0600

    Add files via upload

---
+++ path.js
@@ -0,0 +1,23 @@
+//Your code below
+function buildPath (p,n){
+  var path=[];
+  for(n=5; n<path.length; r++);
+  let r = math.random
+  if(n>p);
+  return true;
+}
+else if (n<=p);
+return fals;
+
+
+
+//Test code
+let testpath1 = buildPath(0.10,15);
+let testpath2 = buildPath(0.50,15);
+console.log(testpath1);
+console.log(testpath2);
+console.log("---------------------");
+console.log(stroll([true,false,true]],0));
+console.log(stroll([true,true,true]],0));
+console.log(rsj(testpath1,0));
+console.log(rsj(testpath2,0));
